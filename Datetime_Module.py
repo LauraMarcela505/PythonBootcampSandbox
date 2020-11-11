06d6d56284d2414a39dde5c4465a8dd678486ad6
lauramgonzalez <Lauramgonzalezc@gmail.com>
2020-11-10 20:18:25 -0500

    Added to subtracting days

--- Datetime_Module.py
+++ Datetime_Module.py
@@ -1,5 +1,6 @@
 import datetime
 
+from datetime import date
 # module for time stamps
 t = datetime.time(4,45,55)
 
@@ -13,3 +14,20 @@ print(t.tzinfo)
 today = datetime.date.today()
 
 print(today)
+
+
+# difference between two date objects
+
+datetime1 = date(2021,11,3)
+datetime2 = date(2020,11,3)
+
+result = datetime1 - datetime2
+
+print(result)
+
+# datetime1 = datetime(2021,11,3,22,0)
+# datetime2 = datetime(2020,11,3,12,0)
+#
+# result = datetime1 -datetime2
+#
+# print(result)
